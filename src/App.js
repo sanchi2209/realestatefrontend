387a1760be083d13f85affbad38bad9f5c371bb4
sanchitha kp <72012948+sanchi2209@users.noreply.github.com>
2023-03-05 17:24:12 +0530

    added frontend files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+//import Search from "./Components/Search"
+import React from 'react';
+// import SearchPage from "./Components/MainPage/SearchPage"
+// import { useState } from 'react';
+import Home from './Components/MainPage/Home';
+function App() {
+  
+  return (
+    <div>
+     {/* <UpperNav/>  */}
+     <Home/>
+   {/* <h1>HiEye</h1> */}
+    </div>
+  );
+}
+
+
+export default App;
